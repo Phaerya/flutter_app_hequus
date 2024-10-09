81a6b48686278606067864daebd169d5b1f6780e
phaerya <joaquim.morais-faria@outlook.com>
2024-10-09 12:23:35 +0200

    HISTORIQUE UN PEU PLUS SYMPA DU REGARD

--- lib/widgets/game_history.dart
+++ lib/widgets/game_history.dart
@@ -37,10 +37,35 @@ class SavedGamesPage extends StatelessWidget {
           itemCount: gameHistory.length,
           itemBuilder: (context, index) {
             final game = gameHistory[index];
-            return ListTile(
-              title: Text("Partie ${index + 1}: ${game['won']}"),
-              subtitle:
-                  Text("Essais: ${game['tries']}, Score: ${game['score']}"),
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.blueAccent, width: 2),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  title: Text(
+                    "Partie ${index + 1}: ${game['won']}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  subtitle: Text(
+                    "Essais: ${game['tries']}, Score: ${game['score']}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
             );
           },
         ),
